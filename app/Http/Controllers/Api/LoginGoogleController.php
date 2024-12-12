b6494218f79eb3df813fd4124a6b18d045f41542
Khoa <133620422+Skrawrz@users.noreply.github.com>
2024-12-12 11:21:32 +0700

    up

--- app/Http/Controllers/Api/LoginGoogleController.php
+++ app/Http/Controllers/Api/LoginGoogleController.php
@@ -20,33 +20,28 @@ class LoginGoogleController extends Controller
     {
         try {
             $user = Socialite::driver('google')->user();
-
-            // Tìm người dùng dựa trên google_id
             $findUser = User::where('google_id', $user->id)->first();
 
             if ($findUser) {
-                // Đăng nhập nếu người dùng đã tồn tại
                 Auth::login($findUser);
                 return redirect()->intended('dashboard');
             } else {
-                // Tạo người dùng mới nếu chưa tồn tại
                 $newUser = User::updateOrCreate(
-                    ['email' => $user->email], // Tìm người dùng dựa trên email
+                    ['email' => $user->email], 
                     [
                         'username' => $user->name,
                         'google_id' => $user->id,
                         'full_name' => $user->name,
                         'role'=>"1",
-                        'password' => bcrypt('123456dummy'), // Sử dụng bcrypt để hash mật khẩu
+                        'password' => bcrypt('123456dummy'),
                     ]
                 );
-
                 Auth::login($newUser);
                 return redirect()->intended('dashboard');
             }
         } catch (\Exception $e) {
             Log::error('Google Login Error: '.$e->getMessage());
-            return redirect()->route('login')->with('error', 'Đăng nhập thất bại, vui lòng thử lại.');
+            return redirect()->route('login')->with('error', 'áĐăng nhập thất bại, vui lòng thử lại.');
         }
     }
 }
