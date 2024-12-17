99e8ccdb37aed94b8069c392ad391be5efe24dd4
Khoa <133620422+Skrawrz@users.noreply.github.com>
2024-12-17 11:33:55 +0700

    up

--- database/migrations/2024_12_16_104717_create_users_table.php
+++ database/migrations/2024_12_16_104717_create_users_table.php
@@ -20,11 +20,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('picture')->nullable();
             $table->boolean('role');
-<<<<<<< HEAD:database/migrations/2014_10_12_000000_create_users_table.php
             $table->string('google_id')->nullable()->unique();
-=======
-            $table->string('google_id')->nullable();
->>>>>>> d2463777561e1bbd21e8c0e80abed4e3881307fd:database/migrations/2024_12_16_104717_create_users_table.php
             $table->rememberToken()->nullable();
             $table->timestamps();
         });
